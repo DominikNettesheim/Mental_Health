ee2a39a799fc56f1b6feffc3f7eb36bea7967f17
royce williams <roycejameswilliams@students.berkeley.net>
2018-05-22 10:00:34 -0700

    hi

--- royce.js
+++ royce.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
     $('#logInBtn').on('click', function(){
         $('#coverflow').hide();
-    })
+    });
+    $('#closeX').on('click', function(){
+        $('#coverflow').show();
+    });
+    $('#signUpBtn').on('click',function(){
+        $('#coverflow').hide();
+    });
+    $('#closeX2').on('click', function(){
+        $('#coverflow').show();
+        });
 });
