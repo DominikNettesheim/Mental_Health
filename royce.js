01d37b0e141e89f90b84523c1bc48463522f07cc
royce williams <roycejameswilliams@students.berkeley.net>
2018-05-25 09:39:46 -0700

    hi

--- royce.js
+++ royce.js
@@ -1,8 +1,16 @@
-var anorexia= "http://www.masteradiccionesonline.com/media/anorexia985-350x350.jpg";
-var adhd ="https://docassistillinois.org/wp-content/uploads/2018/03/ADHD.jpg";
-var depression ="https://s.mxmcdn.net/images-storage/albums4/0/6/1/7/2/6/37627160_350_350.jpg";
-var bipolar ="https://www.cmeoutfitters.com/wp-content/uploads/2017/06/Featured_WC030series.jpg";
-var anxiety = "https://www.traditionalacupuncture.com.au/CMS/wp-content/uploads/anxiety.jpg";
+function Disorder(image,message){
+    this.stories= [];
+    this.image=image;
+    this.message =message;
+    function addStory(story){
+        this.stories.push(story);
+    }
+}
+var anorexia= new Disorder("http://www.masteradiccionesonline.com/media/anorexia985-350x350.jpg");
+var adhd =new Disorder("https://docassistillinois.org/wp-content/uploads/2018/03/ADHD.jpg");
+var depression = new Disorder("https://s.mxmcdn.net/images-storage/albums4/0/6/1/7/2/6/37627160_350_350.jpg");
+var bipolar = new Disorder("https://www.cmeoutfitters.com/wp-content/uploads/2017/06/Featured_WC030series.jpg");
+var anxiety = new Disorder("https://www.traditionalacupuncture.com.au/CMS/wp-content/uploads/anxiety.jpg");
 $(document).ready(function() {
     $('#information').hide();
     $('#logInBtn').on('click', function(){
@@ -24,8 +32,20 @@ $(document).ready(function() {
     $('.photos').dblclick(function(){
         $('#coverflow').fadeOut("slow");
         console.log(this.src);
-        if(this.src == anorexia){
+        if(this.src == anorexia.image){
             console.log("good");
+        }
+        if(this.src ==adhd.image) {
+            console.log("Adhd");
+        }
+        if(this.src ==depression.image) {
+            console.log("alex");
+        }
+        if(this.src ==bipolar.image) {
+            console.log("ty");
+        }
+        if(this.src ==anxiety.image) {
+            console.log("anxst");
         }
         $('#information').html("hello").fadeIn("slow");
     });
