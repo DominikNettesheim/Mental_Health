6f8abba8a6dd80eaebe38cb4009ae7e720bee3f1
royce williams <roycejameswilliams@students.berkeley.net>
2018-05-30 10:33:56 -0700

    hi

--- disorders.js
+++ disorders.js
@@ -11,6 +11,9 @@ function Disorder(image){
     this.addTreatments = function(treatments){
         this.allTreatments=treatments;
     };
+    this.addCauses = function(causes){
+        this.allCauses=causes;
+    };
     this.addStory =function(story){
         this.stories=story;
     };
@@ -50,7 +53,7 @@ function Disorder(image){
         return table;
     }
 
-};
+}
 var cancel = "<span id='closeInfo' class=\"close\" title=\"Close Modal\">&times;</span>";
 
 var anorexia= new Disorder("http://www.masteradiccionesonline.com/media/anorexia985-350x350.jpg");
@@ -61,7 +64,8 @@ var adhd =new Disorder("https://docassistillinois.org/wp-content/uploads/2018/03
 
 var depression = new Disorder("https://s.mxmcdn.net/images-storage/albums4/0/6/1/7/2/6/37627160_350_350.jpg");
 depression.addBackground(["Depression is one of the most common mental illnesses.", "It affects 6.7% (more than 16 million) of American adults each year.","Depression can cause people to lose pleasure from daily life, complicate other medical conditions, and it can even be serious enough to lead to suicide","Only about a third (35.3%) of those suffering from severe depression seek treatment from a mental health professional, although the majority of people with depression have a full remission." , "Many people resist treatment because they believe depression isn't serious, that they can treat it themselves, or that it is a personal weakness rather than a serious medical illness"]);
-
+depression.addCauses(["Biological - People with depression may have too little or too much of certain brain chemicals, called 'neurotransmitters.'  Changes in these brain chemicals may cause or contribute to depression.", "Cognitive - People with negative thinking patterns and low self-esteem are more likely to develop clinical depression.","Gender - More women experience depression than men. The reasons behind this are still unclear, they may include the hormonal changes women go through during menstruation, pregnancy, childbirth and menopause.","Co-occurrence - Depression is more likely to occur along with certain illnesses, such as heart disease, cancer, Parkinson's disease, diabetes, Alzheimer's disease, Multiple Sclerosis, and hormonal disorders.","Medications - Many medications have side effects of depression.","Genetic - A family history of depression increases the risk for developing the illness. Some studies also suggest that a combination of genes and environmental factors work together to increase risk for depression. ", " Situational - Difficult life events, including divorce, financial problems, or the death of a loved one can contribute to depression."]);
+depression.addSymptom(["Persistent sad, anxious or 'empty' mood","Sleeping too much or too little, waking up in the middle of the night or early in the morning.","Reduced appetite and weight loss, or increased appetite and weight gain."]);
 var bipolar = new Disorder("https://www.cmeoutfitters.com/wp-content/uploads/2017/06/Featured_WC030series.jpg");
 
 
