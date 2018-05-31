e21e867a4260558f3ec3f5ae319d604fd607eb3d
Henry Andersen <HenryAndersen@students.berkeley.net>
2018-05-31 09:32:42 -0700

    hen

--- henry.js
+++ henry.js
@@ -1,9 +1,9 @@
+$(document).ready(function(){
 
 
+    console.log(getParameterByName("userId"));
 
-
-
-//
+});
 
 function userCreate() {
     var username = document.getElementById('email').value;
@@ -18,10 +18,40 @@ function userCreate() {
         dataType: 'json',
         success: function(data){
             console.log(data);
+            window.location.replace("index.html?userId=" + data._id);
         },
         error: function(){
             alert("failed");
         },
         url: 'https://slkidsbackend.herokuapp.com/mentalhealthapp/api/users'
     });
+
+}
+
+function login() {
+    var username = document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+    $.ajax({
+        type: 'GET',
+        dataType: 'json',
+        success: function(data){
+            console.log(data);
+        },
+        error: function(){
+            alert("failed");
+        },
+        url: 'https://slkidsbackend.herokuapp.com/mentalhealthapp/api/users/' + username
+    });
+
+}
+
+//get query string parameter from url
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
 }
