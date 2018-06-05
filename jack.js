a87d2c26183c8d9a65ebfc84d9c3b7f3bd2b10d1
Dominik Nettesheim <dominiknetteshein@students.berkeley.net>
2018-06-05 10:13:10 -0700

    coverflow working - albinson

--- jack.js
+++ jack.js
@@ -8,14 +8,6 @@ $(function() {
         }
     });
 
-//   // //$('#coverflow img').click(function() {
-//  //      if( ! $(this).hasClass('ui-state-active')){
-//            return;
-//        }
-//
-//        //$('#coverflow').coverflow('next');
-//
-//
-//    });
+
 
 });
